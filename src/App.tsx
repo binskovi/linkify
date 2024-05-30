ee6ccc0d9b5f4a61b1782f27b001550872fbb7d9
binskovi <kskowronski@bincore.pl>
2024-05-30 19:52:09 +0200

    Environment Initialization - Vite, React, Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="mx-auto my-8 max-w-7xl">
+        <p className='text-red-600 font-bold text-2xl'>Starter linkify 😁</p>
+      </div>
+    </>
+  )
+}
+
+export default App
